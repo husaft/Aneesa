7888763f333b8b164baf174272054a7ab51a07c6
Hubertus Sack <hubertussack@outlook.com>
2015-01-31 07:33:30 +0100

    Added a simple speech recognizer

--- Aneesa/Aneesa.Incubator/Program.cs
+++ Aneesa/Aneesa.Incubator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Speech.Synthesis;
+using System.Speech.Recognition;
 using System.Globalization;
 using System.Configuration;
 
@@ -10,6 +11,7 @@ namespace Aneesa.Incubator
         static void Main(string[] args)
         {
             var synth = new SpeechSynthesizer();
+            synth.SetOutputToDefaultAudioDevice();
 
             var cultureTag = ConfigurationManager.AppSettings["culture"];
             var culture = CultureInfo.GetCultureInfoByIetfLanguageTag(cultureTag);
@@ -20,11 +22,31 @@ namespace Aneesa.Incubator
             prompt.EndSentence();
 
             synth.SpeakAsync(prompt);
-            
+
+            var recog = new SpeechRecognitionEngine(culture);
+            recog.LoadGrammar(new DictationGrammar());
+            recog.SetInputToDefaultAudioDevice();
+            recog.SpeechRecognized += Recog_SpeechRecognized;
+
+            recog.RecognizeAsync(RecognizeMode.Multiple);
+
             Console.WriteLine("Hello!");
             Console.ReadLine();
 
             synth.Dispose();
+            recog.Dispose();
+        }
+
+        private static void Recog_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            var recog = sender as SpeechRecognitionEngine;
+            var args = e.Result;
+            OnSpeechRecognized(recog, args);
+        }
+
+        private static void OnSpeechRecognized(SpeechRecognitionEngine engine, RecognitionResult result)
+        {
+            Console.WriteLine(">> " + result.Text + " / " + result.Confidence);
         }
     }
 }
