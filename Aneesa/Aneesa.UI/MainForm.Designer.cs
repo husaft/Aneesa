dad4a4b711c2a4ede3809794f21d3d55468fc69b
Hubertus Sack <hubertussack@outlook.com>
2015-02-11 00:56:03 +0100

    Changed tools version to 4.0 and the designer changed some line endings or something trivial

--- Aneesa/Aneesa.UI/MainForm.Designer.cs
+++ Aneesa/Aneesa.UI/MainForm.Designer.cs
@@ -28,41 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.statusCircle = new Aneesa.UI.CircleButton();
-            this.statusText = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // statusCircle
-            // 
-            this.statusCircle.Location = new System.Drawing.Point(108, 57);
-            this.statusCircle.Name = "statusCircle";
-            this.statusCircle.Size = new System.Drawing.Size(75, 75);
-            this.statusCircle.TabIndex = 1;
-            // 
-            // statusText
-            // 
-            this.statusText.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.statusText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.statusText.Location = new System.Drawing.Point(12, 146);
-            this.statusText.Name = "statusText";
-            this.statusText.Size = new System.Drawing.Size(270, 20);
-            this.statusText.TabIndex = 2;
-            this.statusText.Text = "What\'s up, yo?";
-            this.statusText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // MainForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(294, 454);
-            this.Controls.Add(this.statusText);
-            this.Controls.Add(this.statusCircle);
-            this.Name = "MainForm";
-            this.Text = "Aneesa";
-            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
-            this.Load += new System.EventHandler(this.MainForm_Load);
-            this.ResumeLayout(false);
-
+        	this.statusCircle = new Aneesa.UI.CircleButton();
+        	this.statusText = new System.Windows.Forms.Label();
+        	this.SuspendLayout();
+        	// 
+        	// statusCircle
+        	// 
+        	this.statusCircle.Location = new System.Drawing.Point(108, 57);
+        	this.statusCircle.Name = "statusCircle";
+        	this.statusCircle.Size = new System.Drawing.Size(75, 75);
+        	this.statusCircle.TabIndex = 1;
+        	// 
+        	// statusText
+        	// 
+        	this.statusText.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+        	this.statusText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+        	this.statusText.Location = new System.Drawing.Point(12, 146);
+        	this.statusText.Name = "statusText";
+        	this.statusText.Size = new System.Drawing.Size(270, 20);
+        	this.statusText.TabIndex = 2;
+        	this.statusText.Text = "What\'s up, yo?";
+        	this.statusText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+        	// 
+        	// MainForm
+        	// 
+        	this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+        	this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        	this.ClientSize = new System.Drawing.Size(294, 454);
+        	this.Controls.Add(this.statusText);
+        	this.Controls.Add(this.statusCircle);
+        	this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+        	this.MaximizeBox = false;
+        	this.Name = "MainForm";
+        	this.Text = "Aneesa";
+        	this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+        	this.Load += new System.EventHandler(this.MainForm_Load);
+        	this.ResumeLayout(false);
         }
 
         #endregion
