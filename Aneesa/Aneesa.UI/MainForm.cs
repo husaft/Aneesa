ed304e88a18a49adf1c091edb0fbfb29cadeffe6
Hubertus Sack <hubertussack@outlook.com>
2015-02-12 01:41:12 +0100

    Introduced collective grammar files to avoid creating a huge number of very small files

--- Aneesa/Aneesa.UI/MainForm.cs
+++ Aneesa/Aneesa.UI/MainForm.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Configuration;
 using System.Diagnostics;
 using System.Drawing;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Security.Principal;
 using System.Speech.Recognition;
+using System.Speech.Recognition.SrgsGrammar;
 using System.Speech.Synthesis;
 using System.Text;
 using System.Windows.Forms;
@@ -38,10 +41,17 @@ namespace Aneesa.UI
 			recognizer.LoadGrammarCompleted += recognizer_LoadGrammarCompleted;
 			recognizer.SpeechRecognized += recognizer_SpeechRecognized;
 			recognizer.SpeechRecognitionRejected += recognizer_SpeechRecognitionRejected;
-			// Get all grammar files and load them
+			// Get all grammar files and read them
 			var currentDir = Environment.CurrentDirectory;
-			var grammarFiles = Directory.GetFiles(currentDir, "*Grammar.xml", SearchOption.AllDirectories);
-			Array.ForEach(grammarFiles, f => recognizer.LoadGrammarAsync(new Grammar(f)));
+			var grammarFiles = Directory.GetFiles(currentDir, "*.cg.xml", SearchOption.AllDirectories);
+			var srgsDocs = grammarFiles.Select(f => new SrgsDocument(f)).ToArray();
+			// Extract all rules
+			var allRules = srgsDocs.SelectMany(d => d.Rules);
+			var privRules = allRules.Where(r => r.Scope == SrgsRuleScope.Private).ToArray();
+			// Build grammars and load them
+			var grammars = srgsDocs.SelectMany(d => d.Rules.Where(r => r.Scope == SrgsRuleScope.Public)
+			                                   .Select(pr => new Grammar(d, pr.Id))).ToArray();
+			Array.ForEach(grammars, g => recognizer.LoadGrammarAsync(g));
 			// Greet the user
 			Speak(SpeakResource.Welcome);
 		}
@@ -66,11 +76,110 @@ namespace Aneesa.UI
 				case "TellName":
 					TellName();
 					break;
+				case "TellLogon":
+					TellLogon();
+					break;
+				case "TellRunning":
+					TellRunning();
+					break;
+				case "TellWinVer":
+					TellWinVer();
+					break;
+				case "TellCPUs":
+					TellCPUs();
+					break;
+				case "TellSpace":
+					TellSpace();
+					break;
+				case "TellUser":
+					TellUser();
+					break;
 				default:
 					Speak(SpeakResource.Sorry);
 					break;
 			}
 		}
+		
+		private void TellUser()
+		{
+			var machine = Environment.MachineName;
+			var identity = WindowsIdentity.GetCurrent().Name.Replace(machine+"\\", string.Empty);
+			Speak(string.Format("You're logged in as {0} on {1}.", identity, machine));
+		}
+		
+		#region Disk space
+		private void TellSpace()
+		{
+			var drives = DriveInfo.GetDrives().Where(d => d.DriveType == DriveType.Fixed).ToArray();
+			var freeSpace = drives.Sum(d => d.AvailableFreeSpace);
+			var diskSpace = drives.Sum(d => d.TotalSize);
+			Speak(string.Format("You've got {0} and {1} of it are free.",
+			                    GetHumanSpace(diskSpace, true), GetHumanSpace(freeSpace, true)));
+		}
+		
+		private static string GetHumanSpace(long bytes, bool longFormat)
+		{
+			string[] sizes;
+			if (longFormat)
+				sizes = new [] { "bytes", "kilobytes", "megabytes", "gigabytes", "terabytes" };
+			else
+				sizes = new [] { "B", "KB", "MB", "GB", "TB" };
+			long tmp;
+			int order = 0;
+			while ((tmp = bytes / 1024L) > 0L)
+			{
+				bytes = tmp;
+				order++;
+			}
+			return string.Format("{0:0.##} {1}", bytes, sizes[order]);
+		}
+		#endregion
+		
+		private void TellCPUs()
+		{
+			var processors = Environment.ProcessorCount;
+			Speak(string.Format("I'm using {0} CPUs.", processors));
+		}
+		
+		private void TellWinVer()
+		{
+			var winver = Environment.OSVersion.VersionString;
+			Speak(string.Format("You've got {0}.", winver), SayAs.NumberOrdinal);
+		}
+		
+		#region Logon & running
+		private void TellLogon()
+		{
+			var date = GetLogonTime().ToString("f", culture);
+			Speak("Your computer started at "+date+".");
+		}
+		
+		private void TellRunning()
+		{
+			var time = CalcPcRunningTime();
+			Speak(string.Format("Your computer is running since {0} hours and {1} minutes.", time.Hours, time.Minutes));
+		}
+		
+		private static TimeSpan CalcPcRunningTime()
+		{
+			return DateTime.Now - GetLogonTime();
+		}
+		
+		private static DateTime GetLogonTime()
+		{
+			return Process.GetProcesses().Select(p => GetStartTime(p)).Min().GetValueOrDefault();
+		}
+		
+		private static DateTime? GetStartTime(Process proc)
+		{
+			try
+			{
+				return proc.StartTime;
+			} catch (Win32Exception) {
+				return null;
+			}
+		}
+		#endregion
 		
 		private void TellName()
 		{
